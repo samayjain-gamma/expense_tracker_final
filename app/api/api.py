809b35f2ceb5a6dda0c3f0dc17707d453f7d7ac3
Samay Jain <samay.jain@gammaedge.io>
2026-02-11 12:00:11 +0530

    authentication, user_registration and login added

---
+++ app/api/api.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+
+from app.api.routes import auth, expenses, budget
+
+
+router = APIRouter()
+
+router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags= ['Auth']
+)
+
+router.include_router(
+    expenses.router,
+    prefix="/expenses",
+    tags=['Expenses']
+)
+
+# router.include_router(
+#     budget.router,
+#     prefix="/budget",
+#     tags=["Budget"]
+# )
