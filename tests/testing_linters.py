5018be6053ef962f41262dd2609d2ea634ac4c3b
Samay Jain <samay.jain@gammaedge.io>
2026-02-11 11:44:10 +0530

    linters with pre-commmit added

---
+++ tests/testing_linters.py
@@ -0,0 +1,12 @@
+# # Write a messy code here
+# # Code is commented, so that mypy and flake8 won't raise an error
+
+# from app import api
+
+
+# def func(a, c, b=2):
+#     print("  hello")
+#     return a + b * c
+
+
+# func(2, 3)
