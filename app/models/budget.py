cec27d3704220b32c78210bfbee5b5a58a77d7e1
Samay Jain <samay.jain@gammaedge.io>
2026-02-13 15:03:51 +0530

    linter and testing added on ci

--- app/models/budget.py
+++ app/models/budget.py
@@ -1,27 +1,27 @@
-import enum
-import uuid
-from datetime import datetime, timezone
+# import enum
+# import uuid
+# from datetime import datetime, timezone
 
-from sqlalchemy import Column, DateTime, Enum, Float, ForeignKey, Integer
-from sqlalchemy.dialects.postgresql import UUID
+# from sqlalchemy import Column, DateTime, Enum, Float, ForeignKey, Integer
+# from sqlalchemy.dialects.postgresql import UUID
 
-# from app.db.base_class import Base
-from app.models.category import expense_category_enum
+# # from app.db.base_class import Base
+# from app.models.category import expense_category_enum
 
 
-class Budget(Base):
-    __tablename__ = "budgets"
+# class Budget(Base):
+#     __tablename__ = "budgets"
 
-    budget_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(
-        Integer,
-        ForeignKey("users.user_id", ondelete="CASCADE"),
-        nullable=False,
-        index=True,
-    )
-    limit = Column(Float, nullable=False)
-    category = Column(expense_category_enum, nullable=False)
-    remaining_limit = Column(Float, nullable=False)
-    created_at = Column(
-        DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
-    )
+#     budget_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+#     user_id = Column(
+#         Integer,
+#         ForeignKey("users.user_id", ondelete="CASCADE"),
+#         nullable=False,
+#         index=True,
+#     )
+#     limit = Column(Float, nullable=False)
+#     category = Column(expense_category_enum, nullable=False)
+#     remaining_limit = Column(Float, nullable=False)
+#     created_at = Column(
+#         DateTime, default=lambda: datetime.now(timezone.utc), nullable=False
+#     )
